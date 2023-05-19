3b08eed27fd958d7aaf07eed049d5aae2e47af63
Deestar <110073733+Deestar@users.noreply.github.com>
2023-05-19 03:55:19 -0700

    just finished with the functionality to allow user send chats now going backend

--- react/src/chatroom/Mobile/chatroom.jsx
+++ react/src/chatroom/Mobile/chatroom.jsx
@@ -2,6 +2,7 @@ import { Header } from "./components/Header";
 import { Chatbody } from "./components/Chatbody";
 import { Footer } from "./components/Footer";
 import "../../assets/css/mobilechat.css";
+import { useCallback, useState } from "react";
 export const MobileChatroom = ({ room }) => {
   return (
     <main className="mobilechat">
--- react/src/chatroom/Mobile/components/Footer.jsx
+++ react/src/chatroom/Mobile/components/Footer.jsx
@@ -1,10 +1,21 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 export const Footer = () => {
   const foot = useRef(null);
   const tarea = useRef(null);
+  const fileInput = useRef(null);
+  //current file name
+  const [fname, setFname] = useState("");
+  //State to either show camera or send arrow
+  const [send, setSend] = useState(false);
+  //state to show textarea
+  const [textset, setTextSet] = useState(true);
+  //Function to blur textarea
   const blurInput = () => {
-    tarea.current.blur();
+    if (tarea.current) {
+      tarea.current.blur();
+    }
   };
+  //Function to blur textarea and maintain a height in 'px'
   const staticheight = () => {
     let h = Math.max(
       document.documentElement.clientHeight,
@@ -14,34 +25,72 @@ export const Footer = () => {
     r.style.setProperty("--height", h + "px");
     document.body.addEventListener("touchstart", blurInput);
   };
+  //Function to set height back to responsive
   const resheight = () => {
+    console.log("blurred");
     let r = document.querySelector(":root");
     r.style.setProperty("--height", "-webkit-fill-available");
     document.removeEventListener("touchstart", blurInput);
   };
+  //Function increase height based on text height
   const incrTextHeight = (e) => {
     let textarea = e.target;
     textarea.style.height = "5.5vh";
     const textHeight = textarea.scrollHeight;
     textarea.style.height = textHeight + "px";
   };
+  //Function to set the send button when user chooses and remove the texarea from view
+  const setImage = (event) => {
+    const { name, type } = event.target.files[0];
+    if (event.target && type.startsWith("image/")) {
+      setFname(name);
+      setSend(true);
+      setTextSet(false);
+    } else {
+      setFname("");
+      setSend(false);
+      setTextSet(true);
+    }
+  };
+  //Function to reset file input and set the textarea
+  const resetInput = () => {
+    setSend(false);
+    setTextSet(true);
+    fileInput.current.value = "";
+  };
+  //Function to send inputs when user clicks send
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+  // prettier-ignore
   return (
-    <footer ref={foot}>
+    <footer ref={foot} onSubmit={handleSubmit}>
       <form>
-        <textarea
+    {textset?    <textarea
+        onKeyUp={(e)=>e.target.value.length > 0?setSend(true):setSend(false)}
           ref={tarea}
           onFocus={staticheight}
-          onInput={incrTextHeight}
+          onInput={(event)=>{
+            incrTextHeight(event)
+
+          }}
           onBlur={resheight}
           placeholder="Type here"
-        ></textarea>
+        >
+        </textarea>
+        :
+        <>
+        <i className="fa-solid fa-xmark" onClick={resetInput}></i>
+        <h3>{fname}</h3>
+        </>
+        }
         <label>
-          <input type="file" />
+          <input type="file" ref={fileInput} onChange={setImage}/>
           <i className="fa-solid fa-camera-retro"></i>
         </label>
-        <button>
+        {send?<button>
           <i className="fa-regular fa-paper-plane"></i>
-        </button>
+        </button>:null}
       </form>
     </footer>
   );
