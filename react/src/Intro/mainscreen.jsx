22d43f12c13a53c1ac88d78d15741ca029685b26
Deestar <110073733+Deestar@users.noreply.github.com>
2023-06-01 01:34:22 -0700

    i just added the mediaquery

--- react/src/Intro/mainscreen.jsx
+++ react/src/Intro/mainscreen.jsx
@@ -1,6 +1,15 @@
 //We are to import react-responsive when i'm ready to build the desktop page
 // ~ for now we return only mobile
+import { useMediaQuery } from "react-responsive";
 import { MobileIndex } from "./mobile/Index";
 export const Introscreen = () => {
-  return <MobileIndex />;
+  const isMobile = useMediaQuery({ maxWidth: 720 });
+  return isMobile ? (
+    <MobileIndex />
+  ) : (
+    <h3>
+      Large screen view is unavailable Kindly switch to your mobile device or
+      screen please
+    </h3>
+  );
 };
--- react/src/chatroom/mainchatscreen.jsx
+++ react/src/chatroom/mainchatscreen.jsx
@@ -1,6 +1,15 @@
 import { MobileChatroom } from "./Mobile/chatroom";
+import { useMediaQuery } from "react-responsive";
 //We are to import react-responsive when i'm ready to build the desktop page
 // ~ for now we return only mobile
 export const MainChatRoom = ({ room }) => {
-  return <MobileChatroom room={room} />;
+  const isMobile = useMediaQuery({ maxWidth: 720 });
+  return isMobile ? (
+    <MobileChatroom room={room} />
+  ) : (
+    <h3>
+      Large screen view is unavailable Kindly switch to your mobile device or
+      screen please
+    </h3>
+  );
 };
